ef5c336ebaca0f7b33851a8d14881d225ec5a977
S.Fujimoto <c0b2312341@edu.teu.ac.jp>
2024-11-19 15:47:50 +0900

    ex6：fps

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    koukaton_img = pg.image.load("fig/3.png")#ex2:こうかとんの読み込み
+    koukaton_img = pg.transform.flip(koukaton_img, True, False)#左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(koukaton_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)#ex5
 
 
 if __name__ == "__main__":
